27b8ea91ba8d05ff968a69ae955058fb64603a2b
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-10-01 17:01:15 +0100

    Change app/javascript/test/setup.js extension to .ts and update teh vitest.config.ts file accordingly

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './app/javascript/tests/setup.js',
+    setupFiles: './app/javascript/tests/setup.ts',
   },
 });
