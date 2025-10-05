8e0953a0663998cfe0787f78a0f27f161f5b58f8
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-10-05 09:52:57 +0100

    Specify parameter type to fix implicit any warning

--- app/javascript/components/custom_hooks/usePicturePuzzle.ts
+++ app/javascript/components/custom_hooks/usePicturePuzzle.ts
@@ -1,6 +1,6 @@
 import {useState, useEffect} from "react";
 
-const usePicturePuzzle = (paramsId) => {
+const usePicturePuzzle = (paramsId: string) => {
   const [puzzle , setPuzzle ] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
