195347210faf5ce3a96f9cb02327d8083d756996
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-10-06 17:38:21 +0100

    Update keys to be camelCase to correctly reflect the update in the API response

--- app/javascript/types/PuzzleType.ts
+++ app/javascript/types/PuzzleType.ts
@@ -3,13 +3,11 @@ import type {TargetNameObjectType} from "./TargetNameObjectType"
 export type PuzzleType = {
   "id": number,
   "title": string,
-  "image_src": string,
-  "task_description": string,
-  "resolution_width":number,
-  "resolution_height": number,
-  "targets": TargetNameObjectType[],
-  "created_at": string,
-  "updated_at": string,
   "imageSrc": string,
-  "taskDescription": string
+  "taskDescription": string,
+  "resolutionWidth":number,
+  "resolutionHeight": number,
+  "targets": TargetNameObjectType[],
+  "createdAt": string,
+  "updatedAt": string,
 }
