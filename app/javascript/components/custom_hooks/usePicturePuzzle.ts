14faa0ef347029380f5197f32bbe5ace9f644f0a
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-10-06 17:54:17 +0100

    Add Type Guard to check for valid API response and specify types for useState hooks

--- app/javascript/components/custom_hooks/usePicturePuzzle.ts
+++ app/javascript/components/custom_hooks/usePicturePuzzle.ts
@@ -1,8 +1,34 @@
 import {useState, useEffect} from "react";
 
+import type { PuzzleType } from "../../types/PuzzleType";
+import type { TargetNameObjectType } from "../../types/TargetNameObjectType";
+
+function isTargetNameObject(data: any): data is TargetNameObjectType {
+  return (
+    data &&
+    typeof data === "object" &&
+    typeof data.name === "string"
+  )
+}
+
+function isPuzzle(data:any): data is PuzzleType {
+  return(
+    data &&
+    typeof data.id === "number" &&
+    typeof data.title === "string" &&
+    typeof data.imageSrc === "string" &&
+    typeof data.taskDescription=== "string" &&
+    typeof data.resolutionWidth === "number" &&
+    typeof data.resolutionHeight === "number" &&
+    Array.isArray(data.targets) && data.targets.every(isTargetNameObject) &&
+    typeof data.createdAt === "string" &&
+    typeof data.updatedAt === "string"
+  )
+}
+
 const usePicturePuzzle = (paramsId: string) => {
-  const [puzzle , setPuzzle ] = useState(null);
-  const [error, setError] = useState(null);
+  const [puzzle , setPuzzle ] = useState<PuzzleType|null>(null);
+  const [error, setError] = useState<Error|null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -19,6 +45,9 @@ const usePicturePuzzle = (paramsId: string) => {
       return response.json();
     })
     .then((response) => {
+      if (!isPuzzle(response)) {
+        throw Error("Invalid puzzle data from API.")
+      }
       setPuzzle(response);
     }).catch((error) => {
       if (error.name !== "AbortError") {
