88909401d00c1d6c74ca9107f04133a1ed19e056
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-10-10 14:40:31 +0100

    Add type guard for API response

--- app/javascript/components/custom_hooks/useGameState.ts
+++ app/javascript/components/custom_hooks/useGameState.ts
@@ -3,6 +3,23 @@ import {useEffect, useState} from "react";
 import type { PuzzleType } from "../../types/PuzzleType";
 import type { IdentifiedTargetType } from "../../types/IdentifiedTargetType";
 
+type APIResponse = {gameFinished: true, secondsToCompletion: number} | {gameFinished: false};
+
+function isAPIResponse(data:any): data is APIResponse {
+  return (
+    (
+      data && 
+      data.gameFinished === true && 
+      typeof data.secondsToCompletion === "number"
+    ) 
+    ||
+    (
+      data && 
+      data.gameFinished === false
+    )
+  )
+}
+
 const useGameState = (puzzle: PuzzleType | null, correctlyIdentifiedTargets: IdentifiedTargetType[], paramsId: string) => {
   const [secondsToCompletion, setSecondsToCompletion] = useState(null);
   const [gameStateError, setGameStateError] = useState<Error | null>(null);
@@ -18,6 +35,9 @@ const useGameState = (puzzle: PuzzleType | null, correctlyIdentifiedTargets: Ide
         }
         return response.json()
       }).then((data) => {
+        if(!isAPIResponse(data)) {
+          throw Error ("Invalid game state response from API.")
+        }
         if (data.gameFinished === true) {
           setSecondsToCompletion(data.secondsToCompletion);
         }
